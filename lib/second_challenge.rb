32056059fc0069a8027398be11385d0687545192
Michael Waldo <mbwcogic@yahoo.com>
2019-09-28 14:42:45 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,21 +1,17 @@
 def second_challenge
-  epic_tragedy    =  {
-     :montague => {
-        :patriarch => {}
-        :matriarch => {}
-        :hero => {},
-        :hero_friends => []
-    #your key/value pairs here!
-      }, 
-      :capulet => {
-         :patriarch => {}
-         :matriarch => {}
-         :heroine   => {}
-         :heroine_friends => []
-      #your key/value pairs here!
-      }
-    
-  }
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
-  
 end
