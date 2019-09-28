97cf53450c76078234d5d899eae9916ebd3f6fe8
Michael Waldo <mbwcogic@yahoo.com>
2019-09-28 14:59:20 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,15 +2,32 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name => "Lord Montague"
+        :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Montague"
+        :age => "54"
         #your key/value pairs here
         },
       :hero => {
+        :name = "Romeo",
+        :age => "15"
         #your key/value pairs here
         },
-      :hero_friends => []
+      :hero_friends => [ 
+        {   
+           :name => "Benvolio",
+           :age => "17",
+           :attitude => "worried"
+        },
+        {
+           :name => "Mercutio",
+           :age => "18"
+           :attitude => "hot-heaed"
+        }
+     ]
    }, 
    :capulet => {
       :patriarch => {
